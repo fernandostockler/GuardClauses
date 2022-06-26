39fe3daa717d55d7242284c8a062a1f3330f6377
Fernando Stockler de Lima <fernando.stockler@outlook.com.br>
2022-06-26 19:54:56 -0300

    Refactor for FluentAssertions

--- GuardClauses/Extensions/ArgumentOutOfRangeExceptionExtensions.cs
+++ GuardClauses/Extensions/ArgumentOutOfRangeExceptionExtensions.cs
@@ -16,10 +16,10 @@ public static partial class GuardClausesExtensions
         string? message = null) where T : IComparable, IComparable<T>
     {
         if (minimunValue.CompareTo(maximunValue) > 0)
-            throw new ArgumentException($"Minimun value ({minimunValue}) must be below or equal maximun value ({maximunValue}).", nameof(minimunValue));
+            throw new ArgumentException($"Minimun value ({minimunValue}) must be below or equal maximun value ({maximunValue}).", paramName);
 
         if (message is null)
-            message = $"Input ({input}) was out of range. Minimun: {minimunValue}, Maximun: {maximunValue}";
+            message = $"Input ({input}) was out of range. Minimun: {minimunValue}, Maximun: {maximunValue}.";
 
         if (input.CompareTo(minimunValue) < 0 || input.CompareTo(maximunValue) > 0)
             throw new ArgumentOutOfRangeException(nameof(input), input, message);
@@ -35,7 +35,7 @@ public static partial class GuardClausesExtensions
         string? message = null) where T : IComparable, IComparable<T>
     {
         if (minimunValue.CompareTo(maximunValue) > 0)
-            throw new ArgumentException($"Minimun value ({minimunValue}) must be below or equal maximun value ({maximunValue}).", nameof(minimunValue));
+            throw new ArgumentException($"Minimun value ({minimunValue}) must be below or equal maximun value ({maximunValue}).", paramName);
 
         foreach (T value in values.Where(value =>
             value.CompareTo(minimunValue) < 0 || value.CompareTo(maximunValue) > 0))
@@ -43,7 +43,7 @@ public static partial class GuardClausesExtensions
             if (message is null)
                 message = $"Input ({value}) was out of range. Minimun: {minimunValue}, Maximun: {maximunValue}";
 
-            throw new ArgumentOutOfRangeException(nameof(values), values, message);
+            throw new ArgumentOutOfRangeException(nameof(values), value, message);
         }
 
         return values;
--- UnityTests/ArgumentOutOfRangeExceptionTests.cs
+++ UnityTests/ArgumentOutOfRangeExceptionTests.cs
@@ -1,51 +1,84 @@
 ﻿namespace UnitTests;
 
+using GuardClauses.Extensions;
+using FluentAssertions;
+using static FluentAssertions.FluentActions;
 public class ArgumentOutOfRangeExceptionTests
 {
-    [TestCase(3)]
-    [TestCase(4)]
-    [TestCase(11)]
-    [TestCase(0)]
-    public void OutOfRangeTestWithInvalidInput(int number)
+    [TestCase(5, 4, 3)]
+    [TestCase(5f, 4f, 3f)]
+    [TestCase(5d, 4d, 3d)]
+    public void OutOfRangeTestWithMinimumGraterThanMaximun<T>(T min, T max, T value)
+        where T : IComparable, IComparable<T>
     {
-        _ = Assert.Throws<ArgumentOutOfRangeException>(
-            () => Guard.Against.OutOfRange(number, 5, 10, "testParm"));
+        _ = Invoking(() => Guard.Against.OutOfRange(value, min, max, "minimunValue"))
+                .Should().Throw<ArgumentException>()
+                .WithMessage($"Minimun value ({min}) must be below or equal maximun value ({max}). (Parameter 'minimunValue')")
+                .WithParameterName("minimunValue");
     }
 
-    [TestCase(5)]
-    [TestCase(6)]
-    [TestCase(7)]
-    [TestCase(8)]
-    [TestCase(9)]
-    [TestCase(10)]
-    public void OutOfRangeTestWithValidInput(int number)
+    [TestCase(5, 10, 3)]
+    [TestCase(5f, 10f, 11f)]
+    [TestCase(5d, 10d, 4d)]
+    public void OutOfRangeTestWithValueOutOfRange<T>(T min, T max, T value)
+        where T : IComparable, IComparable<T>
     {
-        Assert.DoesNotThrow(
-            () => Guard.Against.OutOfRange(number, 5, 10, "testParm"));
+        _ = Invoking(() => Guard.Against.OutOfRange(value, min, max, "input"))
+                .Should().Throw<ArgumentOutOfRangeException>()
+                .WithMessage($"Input ({value}) was out of range. Minimun: {min}, Maximun: {max}. (Parameter 'input'){Environment.NewLine}Actual value was {value}.")
+                .WithParameterName("input");
     }
 
-    [Test]
-    public void OutOfRangeTestWithMinMoreThanMax()
-    => _ = Assert.Throws<ArgumentException>(() =>
-        Guard.Against.OutOfRange(15, 50, 10, "testParm"));
+    [TestCase(5, 10, 5)]
+    [TestCase(5f, 10f, 10f)]
+    [TestCase(5d, 10d, 6d)]
+    public void OutOfRangeTestWithValidValue<T>(T min, T max, T value)
+        where T : IComparable, IComparable<T>
+    {
+        _ = Invoking(() => Guard.Against.OutOfRange(value, min, max, "input"))
+                .Should().NotThrow();
+    }
 
-    [Test]
-    public void OutOfRangeTestWithCollectionAndBadData()
-    => _ = Assert.Throws<ArgumentOutOfRangeException>(() =>
-        Guard.Against.OutOfRange(new List<int> { 1, 2, 3 }, 5, 10, "testParm"));
+    [TestCase(1, 2, 6, 0, 5, 6)]
+    [TestCase(-2, 2, 5, 0, 5, -2)]
+    [TestCase(1.3, 2.4, 5.1, 0.0, 5.0, 5.1)]
+    [TestCase(-2.0, 2.1, 5, 0.0, 5.0, -2.0)]
+    [TestCase(1.3f, 2.4f, 7.5f, 0.0f, 5.0f, 7.5f)]
+    [TestCase(-1.0f, 2.1f, 5.0f, 0.0f, 5.0f, -1.0f)]
+    public void OutOfRangeTestWithOutOfRangeItemInList<T>(T Item0, T Item1, T Item2, T min, T max, T actual)
+        where T : IComparable, IComparable<T>
+    {
+        List<T> list = new() { Item0, Item1, Item2 };
 
-    [Test]
-    public void OutOfRangeTestWithCollectionAndGoodData()
-    => Assert.DoesNotThrow(() =>
-        Guard.Against.OutOfRange(new List<int> { 6, 8, 10 }, 5, 10, "testParm"));
+        _ = Invoking(() => Guard.Against.OutOfRange(list, min, max, "values"))
+            .Should().Throw<ArgumentOutOfRangeException>()
+            .WithMessage($"Input ({actual}) was out of range. Minimun: {min}, Maximun: {max} (Parameter 'values'){Environment.NewLine}Actual value was {actual}.")
+            .WithParameterName("values");
+    }
 
-    [Test]
-    public void OutOfRangeTestWithCollectionAndMinMoreThanMax()
-    => _ = Assert.Throws<ArgumentException>(() =>
-        Guard.Against.OutOfRange(new List<int> { 1, 40, 60 }, 50, 10, "testParm"));
+    [TestCase(1, 2, 3, 0, 5)]
+    [TestCase(0, 2, 5, 0, 5)]
+    [TestCase(1.3, 2.4, 3.5, 0.0, 5.0)]
+    [TestCase(0, 2.1, 5, 0.0, 5.0)]
+    [TestCase(1.3f, 2.4f, 3.5f, 0.0f, 5.0f)]
+    [TestCase(0f, 2.1f, 5.0f, 0.0f, 5.0f)]
+    public void OutOfRangeTestWithInRangeItemsInList<T>(T Item0, T Item1, T Item2, T min, T max)
+        where T : IComparable, IComparable<T>
+    {
+        var list = new List<T>() { Item0, Item1, Item2 };
 
-    [Test]
-    public void OutOfRangeTestWithCollectionAndMaxLessThanMin()
-    => _ = Assert.Throws<ArgumentException>(() =>
-        Guard.Against.OutOfRange(new List<int> { -5, -1, 0, 5 }, 0, -5, "testParm"));
+        _ = Invoking(() => Guard.Against.OutOfRange(list, min, max, "values"))
+            .Should().NotThrow();
+    }
+
+    [TestCase(1, 2, 3, 4, 3)]
+    [TestCase(-3, -2, 0, 0, -3)]
+    public void OutOfRangeTestWithMinimumGraterThanMaximunInList<T>(T Item0, T Item1, T Item2, T min, T max)
+        where T : IComparable, IComparable<T>
+    {
+        _ = Invoking(() => Guard.Against.OutOfRange(values: new List<T>() { Item0, Item1, Item2 }, minimunValue: min, maximunValue: max, paramName: "values"))
+            .Should().Throw<ArgumentException>()
+            .WithMessage($"Minimun value ({min}) must be below or equal maximun value ({max}). (Parameter 'values')")
+            .WithParameterName("values");
+    }
 }
